ca47d446df3821080c701822d8be81b83492a6bd
btcon7 <98955935+btcon7@users.noreply.github.com>
2022-07-22 12:35:14 +0200

    Add files via upload

---
+++ rainbow interactive/Interactive-Rainbow-main/README.md
@@ -0,0 +1,2 @@
+# Interactive Rainbow
+ creates an interactive rainbow
---
+++ rainbow interactive/Interactive-Rainbow-main/js/javscript.js
@@ -0,0 +1,45 @@
+
+// The functions below gets the ID from the HTML file and sets the div to red and makes the text of the color appear on click
+function redColour()
+{
+    //sets div background to red onclick
+    document.getElementById("red").style.backgroundColor ="red";
+
+    //sets div text to appear on click
+    document.getElementById("red").innerHTML="red";
+}
+
+function orangeColour()
+{
+    document.getElementById("orange").style.backgroundColor ="orange";
+    document.getElementById("orange").innerHTML="orange";
+}
+
+function yellowColour()
+{
+    document.getElementById("yellow").style.backgroundColor ="yellow";
+    document.getElementById("yellow").innerHTML="yellow";
+}
+
+function greenColour()
+{
+    document.getElementById("green").style.backgroundColor ="green";
+    document.getElementById("green").innerHTML="green";
+}
+
+function blueColour()
+{
+    document.getElementById("blue").style.backgroundColor ="blue";
+    document.getElementById("blue").innerHTML="blue";
+}
+function purpleColour()
+{
+    document.getElementById("purple").style.backgroundColor ="purple";
+    document.getElementById("purple").innerHTML="purple";
+}
+
+function lavenderColour()
+{
+    document.getElementById("lavender").style.backgroundColor ="#967bb6";
+    document.getElementById("lavender").innerHTML="lavender";
+}
